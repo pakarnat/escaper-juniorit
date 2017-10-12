e3d00772b339f2ba492f277cc92d48f6b6630b9c
xCldx <32733997+xCldx@users.noreply.github.com>
2017-10-12 14:07:42 +0300

    Playermovement kommentoitu
    kommentoitu playermovement scriptiä

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+
+    //Hiiren liikkumis muuttujat
     float yRotation;
     float xRotation;
-    float lookSensitivity = 5;
+    public float lookSensitivity = 5;
     float currentXRotation;
     float currentYRotation;
     float yRotationV;
     float xRotationV;
     float lookSmoothnes = 0.1f;
 
+    //Pelaajan liikkuminen
     public float walkSpeed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float runSpeed = 8.0F;
     public float gravity = 20.0F;
+
 
     private Vector3 moveDirection = Vector3.zero;
     private CharacterController controller;
@@ -27,6 +31,7 @@ public class PlayerMovement : MonoBehaviour {
 
     void Update()
     {
+        //pelaaja on maassa niin silloin liikkuminen / hyppääminen
         if (controller.isGrounded)
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
@@ -35,8 +40,11 @@ public class PlayerMovement : MonoBehaviour {
             if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
         }
+        //liikkuminen wsad
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+
+        //hiiren controllit
         yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
         xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
         xRotation = Mathf.Clamp(xRotation, -80, 100);
